8c95b333106b4ea6bfee11ae8d7cf27a26f4b9c8
Pedro Lizarraga <plizarraga86@gmail.com>
2016-09-26 17:56:51 -0700

    Se agrega funcion para inicializar la app

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -1,42 +1,48 @@
 angular.module('weatherApp', [])
-  .controller('WeatherController', function($scope, $http) {
-	$scope.cityName = 'Mexicali, Baja California';
-	$scope.location = '';
-	$scope.foodCategory = 'Sushi';
-	$scope.foodCategoryList = ['Sushi', 'Mexican','Italian', 'Fast Food', 'Coffe Shop', 'Japanese'];
-	$scope.weatherList = [];
+.controller('WeatherController', function($scope, $http) {
+
+	$scope.init = function(){
+		$scope.cityName = 'Mexicali, Baja California';
+		$scope.location = '';
+		$scope.foodCategory = 'Sushi';
+		$scope.foodCategoryList = ['Sushi', 'Mexican','Italian', 'Fast Food', 'Coffe Shop', 'Japanese'];
+		$scope.weatherList = [];
+		$scope.getWeather();
+	};
 
 	$scope.getWeather = function(){
 		url = "https://query.yahooapis.com/v1/public/yql?q=select * from weather.forecast where woeid in (select woeid from geo.places(1) where text=\""+$scope.cityName+"\")&format=json&env=store://datatables.org/alltableswithkeys"
 		$http.get(url)
-			.success(function(data) {
-				console.log(data);
-				if(data.query.results){
-					var loc = data.query.results.channel.location;
-					$scope.location = loc.city + ", " + loc.region + ", " + loc.country;
+		.success(function(data) {
+			console.log(data);
+			if(data.query.results){
+				var loc = data.query.results.channel.location;
+				$scope.weatherList = data.query.results.channel.item.forecast;
+				$scope.location = loc.city + ", " + loc.region + ", " + loc.country;
 					// @todo - mostar el clima de 5 dias
 				}else{
+					$scope.location = ''
 					console.log('Please enter a valid location');					
 				}
 			})
-			.error(function(data) {
-				console.log('Error:' + data);
-			});
+		.error(function(data) {
+			console.log('Error:' + data);
+		});
 	};
 
 	$scope.getRestaurants = function(){
 		url = "https://query.yahooapis.com/v1/public/yql?q=select * from local.search where query=\""+$scope.foodCategory+"\" and location=\""+$scope.cityName+"\"&format=json&env=store://datatables.org/alltableswithkeys"
 		$http.get(url)
-			.success(function(data) {
-				console.log(data);
-				if(data.query.results){
+		.success(function(data) {
+			console.log(data);
+			if(data.query.results){
 					// @todo - renderear los restaurantes
 				}else{
 					console.log("No restaurants found")
 				}
 			})
-			.error(function(data) {
-				console.log('Error:' + data);
-			});
+		.error(function(data) {
+			console.log('Error:' + data);
+		});
 	};
-  });
+});
