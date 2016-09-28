34217b51d08feace4f2b71950fcbea1422fe369f
plizarraga <plizarraga86@gmail.com>
2016-09-27 22:43:39 -0700

    Se agregan tests para el API con mochaJS

---
+++ test/weather.test.js
@@ -0,0 +1,27 @@
+var expect = require('chai').expect;
+var request = require('superagent');
+var assert = require('assert');
+
+var urlAPI = 'http://localhost:8000';
+
+describe('Weather API', function(){
+	describe('when requested at /wheawter/san diego', function(){
+		it('expect city: San Diego', function(done){
+			request.get(urlAPI+'/weather/san diego').end(function(err, res){
+				expect(res).to.have.property('status', 200);
+				expect(res.body.query.results.channel.location.city).to.equal('San Diego');
+				done();
+			});
+		});
+	});
+
+	describe('when requested at /restaurants/san diego/italian', function(){
+		it('expect 10 restaurants', function(done){
+			request.get(urlAPI+'/restaurants/san diego/italian').end(function(err, res){
+				expect(res).to.have.property('status', 200);
+				expect(res.body.query.results.Result.length).to.equal(10);
+				done();
+			});
+		});
+	});
+});
